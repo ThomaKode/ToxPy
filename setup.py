05eb408999f3e7cfb451d36288c98e1b80d20e8e
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-09-21 22:21:54 +0000

    Added the first set of commands to the setup file  - Used for uploading to Pypi source

---
+++ setup.py
@@ -0,0 +1,15 @@
+﻿
+from django.core.mail.backends import console
+
+class iter_1(object), tuple_iterator): iter_1 isinstance (object, classmethod-or-type-or-tuple)
+           
+ for(i= 0  : object != i ) license
+       
+    
+    def console
+        .print(EnvironmentError)
+    pass
+
+
+
+
