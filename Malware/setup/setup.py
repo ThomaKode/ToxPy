31d62380514e20985d602466687cb98b2296986e
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-09-22 01:47:14 +0000

    Update file setup.py

---
+++ Malware/setup/setup.py
@@ -0,0 +1,20 @@
+﻿
+import setuptools
+from setuptools.archive_util import os 
+from django.core.mail.backends import console
+
+class iter_1(object), tuple_iterator): iter_1 isinstance (object, classmethod-or-type-or-tuple)
+  """The first in a set iterations for each function""" 
+          
+ for(i= 0  : object != i ) bit_length (type = 'option');
+  
+       if (builtin_function_or_method is not == 'option') 
+           UserWarning(console) 
+
+    def console
+        .print(EnvironmentError)
+    pass
+
+
+
+
